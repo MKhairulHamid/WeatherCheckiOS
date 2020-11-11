0336418989987e0d9832d907153c8ebd590b4ce6
MKhairulHamid <mkhairulhamid@gmail.comn>
2020-11-12 01:24:05 +0700

    Initial Commit

---
+++ WeatherCheck/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatherCheck
+//
+//  Created by M Khairul Hamid on 12/11/20.
+//  Copyright © 2020 M Khairul Hamid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
